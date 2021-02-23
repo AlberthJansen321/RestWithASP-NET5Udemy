fb3b7c7a7bf192dd856b7d4ba80f3f826b1dd773
AlberthJansen321 <75434559+AlberthJansen321@users.noreply.github.com>
2021-02-23 17:11:21 -0300

    adicionando os models parson

---
+++ RestWithASPNET5Udemy/RestWithASPNET5Udemy/Models/Parson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Models
+{
+    public class Parson
+    {
+        public long Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
